d2fc527593d30666e435455a83c9c005332f5631
Thomas Tych <thomas.tych@gmail.com>
2024-01-21 22:38:55 +0100

    Update tailwind to v3.4.1

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,5 +1,10 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./source/**/*.erb"],
+    content: [
+        "./source/**/*.{erb,html,js}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
